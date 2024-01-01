d79405a22e3924a2c1b64ec1d5836ae672957b05
NAnkita24 <apjagtap24@gmail.com>
2024-01-01 11:29:56 +0530

    Initial Basic DSA That I had Completed year ago.

---
+++ src/statement/ForDemo.java
@@ -0,0 +1,21 @@
+package statement;
+
+import java.awt.geom.Area;
+import java.util.Scanner;
+
+public class ForDemo {
+	ForDemo(){
+		
+	}
+	
+    ForDemo(int a,int d){
+		int c=a+d;
+	}
+	public static void main(String[] args) {
+		ForDemo f1=new ForDemo();
+		ForDemo f21=new ForDemo(33,22);
+ 
+	
+}
+
+}
